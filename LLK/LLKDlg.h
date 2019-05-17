2fc67e7ebdf5da4da1ed2b76d6cd1677e7469218
hao <842658636@qq.com>
2019-05-17 10:01:45 +0800

    游戏开始界面

---
+++ LLK/LLKDlg.h
@@ -0,0 +1,38 @@
+﻿
+// LLKDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CLLKDlg 对话框
+class CLLKDlg : public CDialogEx
+{
+// 构造
+public:
+	CLLKDlg(CWnd* pParent = nullptr);	// 标准构造函数
+	
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LLK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	CDC m_dcMem;//位图内存
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+	//初始化背景DC
+	void InitBackground();
+};
